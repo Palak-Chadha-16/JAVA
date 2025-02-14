b8dab0ca47d9788e5437b544c34d1c914327499f
Palak-Chadha-16 <palaktush@gmail.com>
2025-02-14 12:34:33 +0530

    Add files via upload

---
+++ Assignment 1/Print Line JAVA.java
@@ -0,0 +1,5 @@
+class PrintStatement {
+    public static void main(String[] args) {
+        System.out.println("\"Can you print '\\\\' with System.out.println statement?\"");
+    }
+}
---
+++ Assignment 1/employeedata.java
@@ -0,0 +1,12 @@
+public class EmployeeData {
+    public static void main(String[] args) {
+        // Printing employee details
+        System.out.println("Name     | Email                | Employee Code | Phone Number");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Aryan    | aryan@company.com    | E1001         | 9876500001");
+        System.out.println("Rohan    | rohan@company.com    | E1002         | 9876500002");
+        System.out.println("Neha     | neha@company.com     | E1003         | 9876500003");
+        System.out.println("Sanya    | sanya@company.com    | E1004         | 9876500004");
+        System.out.println("Karan    | karan@company.com    | E1005         | 9876500005");
+    }
+}
